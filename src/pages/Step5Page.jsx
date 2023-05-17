10b5ae10e0222a2fbccab5c12f57f53e804136b6
Brandon Artigue <bartigue87@yahoo.com>
2023-05-16 18:18:05 -0600

    add step 5

--- src/pages/Step5Page.jsx
+++ src/pages/Step5Page.jsx
@@ -1,9 +1,17 @@
-import SideStepper from "../components/SideStepper";
+import check from "../assets/Check.png";
 
 export default function Step5Page() {
   return (
     <>
-      <SideStepper activeNumber={4} />
+      <div className="step5--container">
+        <img style={{ height: "80px", width: "80px" }} src={check} />
+        <h1>Thank You!</h1>
+        <p>
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@loremgaming.com.
+        </p>
+      </div>
     </>
   );
 }
